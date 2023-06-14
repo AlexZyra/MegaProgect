0be5175503f88057072489ce7984b22eccd65de4
Alex Zyra <117516145+AlexZyra@users.noreply.github.com>
2023-06-14 12:35:37 +0300

    Add files via upload

--- Tech Shop/js/app.js
+++ Tech Shop/js/app.js
@@ -2,7 +2,7 @@ document.getElementById("logIn").onclick = addElement;
 function addElement() {
     var newDiv = document.createElement("div");
     newDiv.classList.add("logMenu");
-    newDiv.innerHTML = "\n    <p>Text </p>\n    <div id=\"regisClose\" onclick=\"newDiv.handleClear()\">\n      <img src=\"ts/cross.svg\" alt=\"a\">\n    </div>\n  ";
+    newDiv.innerHTML = "\n    <div id=\"regisClose\" onclick=\"newDiv.handleClear()\">\n      <img src=\"ts/cross.svg\" alt=\"a\">\n    </div>\n    <div id=\"regisMenu\">\n    <div id=\"regisMenuTop\"></div>\n      <form action=\"\" method=\"post\">\n        <p>Enter your name</p>\n        <div class=\"input-container\">\n          <input type=\"text\" id=\"input\" required=\"\">\n          <label for=\"input\" class=\"label\">Enter name</label>\n        <div class=\"underline\"></div>\n        </div>\n        <p>Enter your email</p>\n        <div class=\"input-container\">\n          <input type=\"email\" id=\"input\" required=\"\">\n          <label for=\"input\" class=\"label\">Enter email</label>\n        <div class=\"underline\"></div>\n        </div>\n        <p>Enter your password</p>\n        <div class=\"input-container\">\n          <input type=\"password\" id=\"parol\" required=\"\">\n          <label for=\"input\" class=\"label\">Enter password</label>\n        <div class=\"underline\"></div>\n        </div>\n        \n        <p>Re-enter your password</p>\n        <div class=\"input-container\">\n          <input type=\"password\" id=\"input\" required id=\"confirm-password\" value=\"\">\n          <label for=\"input\" class=\"label\">Re-enter password</label>\n        <div class=\"underline\"></div>\n        </div>\n        <input type=\"submit\" id=\"submit\">\n      </form>\n      <button id=\"activePassword\">Show password</button>\n    </div>\n    \n  ";
     newDiv.style.visibility = 'visible';
     var currentDiv = document.getElementById("div1");
     document.body.insertBefore(newDiv, currentDiv);
@@ -11,6 +11,29 @@ function addElement() {
         newDiv.innerHTML = '';
         newDiv.style.visibility = 'hidden';
     }
+    var check = document.getElementById("activePassword");
+    var parol = document.getElementById("parol");
+    check.onclick = function show() {
+        if (parol.getAttribute('type') == 'password') {
+            parol.removeAttribute('type');
+            parol.setAttribute('type', 'text');
+            check.style.backgroundColor = "rgb(255, 70, 70)";
+        }
+        else {
+            parol.removeAttribute('type');
+            parol.setAttribute('type', 'password');
+            check.style.backgroundColor = "rgb(61, 170, 61)";
+        }
+    };
+    // let validInput: any = document.getElementById("submit")
+    // function validateForm(event: any):void {
+    //   event.preventDefault();
+    //   if (password.value !== confirmPassword.value) {
+    //     alert("Вы ввели неправильный повторный пароль.");
+    //   } else {
+    //     alert("Вы успешно зареегестрированы")
+    //   }
+    // }
 }
 var activeMenu = document.getElementById("menu");
 activeMenu.onclick = function () {
--- Tech Shop/ts/app.ts
+++ Tech Shop/ts/app.ts
@@ -3,20 +3,75 @@ function addElement() {
   const newDiv = document.createElement("div");
   newDiv.classList.add("logMenu");
   newDiv.innerHTML = `
-    <p>Text </p>
     <div id="regisClose" onclick="newDiv.handleClear()">
       <img src="ts/cross.svg" alt="a">
     </div>
+    <div id="regisMenu">
+    <div id="regisMenuTop"></div>
+      <form action="" method="post">
+        <p>Enter your name</p>
+        <div class="input-container">
+          <input type="text" id="input" required="">
+          <label for="input" class="label">Enter name</label>
+        <div class="underline"></div>
+        </div>
+        <p>Enter your email</p>
+        <div class="input-container">
+          <input type="email" id="input" required="">
+          <label for="input" class="label">Enter email</label>
+        <div class="underline"></div>
+        </div>
+        <p>Enter your password</p>
+        <div class="input-container">
+          <input type="password" id="parol" required="">
+          <label for="input" class="label">Enter password</label>
+        <div class="underline"></div>
+        </div>
+        
+        <p>Re-enter your password</p>
+        <div class="input-container">
+          <input type="password" id="input" required id="confirm-password" value="">
+          <label for="input" class="label">Re-enter password</label>
+        <div class="underline"></div>
+        </div>
+        <input type="submit" id="submit">
+      </form>
+      <button id="activePassword">Show password</button>
+    </div>
+    
   `;
   newDiv.style.visibility = 'visible';
   const currentDiv = document.getElementById("div1");
   document.body.insertBefore(newDiv, currentDiv);
   document.getElementById("regisClose").onclick = handleClear;
-  function handleClear(){
+  function handleClear() {
     newDiv.innerHTML = '';
     newDiv.style.visibility = 'hidden';
   }
+  let check = document.getElementById("activePassword");
+  let parol = document.getElementById("parol");
 
+  check.onclick = function show() {
+    if (parol.getAttribute('type') == 'password') {
+      parol.removeAttribute('type');
+      parol.setAttribute('type', 'text');
+      check.style.backgroundColor = "rgb(255, 70, 70)";
+    } else {
+      parol.removeAttribute('type');
+      parol.setAttribute('type', 'password');
+      check.style.backgroundColor = "rgb(61, 170, 61)";
+    }
+
+  }
+  // let validInput: any = document.getElementById("submit")
+  // function validateForm(event: any):void {
+  //   event.preventDefault();
+  //   if (password.value !== confirmPassword.value) {
+  //     alert("Вы ввели неправильный повторный пароль.");
+  //   } else {
+  //     alert("Вы успешно зареегестрированы")
+  //   }
+  // }
 }
 
 
